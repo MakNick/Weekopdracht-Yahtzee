d2c141dc2af8682ba322d1d52b8e88f3bf3ed296
MakNick <nick_makkinga@hotmail.com>
2018-10-18 11:05:54 +0200

    18.10.2018

--- Speler.java
+++ Speler.java
@@ -4,13 +4,14 @@ import java.util.ArrayList;
 
 public class Speler {
 	ArrayList<Worp> worpGeschiedenis = new ArrayList<Worp>();
+	String naam;
 	int aantalWorpen;
 	final int maxAantalWorpen = 3;
-	
-	void toonWorpGeschiedenis(){
+		
+	void toonWorpGeschiedenis(int a, int b){
 		System.out.println("");
 		for(int x = 0; x < worpGeschiedenis.size(); x++){
-			System.out.print("Geschiedenis, worp " + (x + 1) + ": ");
+			System.out.print("\nGeschiedenis van " + YahtzeeSpel.spelers.get(a).naam + ", beurt " + (b + 1) + ": ");
 			for(int j = 0; j < 5; j++){
 				System.out.print("\t" + worpGeschiedenis.get(x).worpUitslag[j]);
 			}
--- YahtzeeSpel.java
+++ YahtzeeSpel.java
@@ -4,52 +4,66 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class YahtzeeSpel {
+	static Scanner sc = new Scanner(System.in);
 	ArrayList<Dobbelsteen> dobbelstenen = new ArrayList<>();
-	ArrayList<Speler> spelers = new ArrayList<>();
+	static ArrayList<Speler> spelers = new ArrayList<>();
 	int[] blokkeerArray = {0,0,0,0,0};
 	int iteratorBlokkeerArray;
-	Speler speler = new Speler();
 	boolean doorspelen = true;
+	int maxAantalBeurten = 13;
 	
 	YahtzeeSpel(){
 		for(int i = 0; i < 5; i++){
 			dobbelstenen.add(new Dobbelsteen());
 		}
 	}
-	
+		
 	void spelen(){
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Welkom bij Yahtzee!\nDruk op enter om te spelen, druk op 'q' om te stoppen.");
-		String invoer = sc.nextLine();
-			if(invoer.equals("q")){
-				doorspelen = false;
-			}
+		toevoegenSpelers();
+//		System.out.println("\nDruk op enter om te spelen, druk op 'q' om te stoppen.");
+//		String invoer = sc.nextLine();
+//			if(invoer.equals("q")){
+//				doorspelen = false;
+//			}
 
 		while(doorspelen) {	
-			while(speler.aantalWorpen < speler.maxAantalWorpen){
-				werpDobbelstenen();
-				System.out.println("\nDruk op 'p' om de worp op te slaan.\n");
-				System.out.println("of selecteer welke dobbelstenen je wilt vasthouden.\nType '0' voor geen, anders bijvoorbeeld positie '13'");
-				invoer = sc.next();
-				if(invoer.equals("p")){
-					opslaanWorp();
-					speler.toonWorpGeschiedenis();
-					speler.aantalWorpen = 0;
-					System.out.println("");
-				} else {
-					vasthouden(invoer);
+			for(int beurtenIndex = 0; beurtenIndex < maxAantalBeurten; beurtenIndex++){
+				for(int spelerIndex = 0; spelerIndex < spelers.size(); spelerIndex++){
+					spelers.get(spelerIndex).aantalWorpen = 0;
+					System.out.println(">" + spelers.get(0).aantalWorpen);
+					System.out.println(">" + spelers.get(1).aantalWorpen);
+
+					while(spelers.get(spelerIndex).aantalWorpen < spelers.get(spelerIndex).maxAantalWorpen){
+						System.out.println("X" + spelers.get(0).aantalWorpen);
+						System.out.println("X" + spelers.get(1).aantalWorpen);
+						if(spelers.get(spelerIndex).aantalWorpen == 2){
+							werpDobbelstenen(spelerIndex, beurtenIndex);
+							opslaanWorp(spelerIndex);
+							spelers.get(spelerIndex).toonWorpGeschiedenis(spelerIndex, beurtenIndex);
+							spelers.get(spelerIndex).aantalWorpen = 3;
+							System.out.println("");
+						} else {
+							werpDobbelstenen(spelerIndex, beurtenIndex);
+							System.out.println("\nDruk op 'p' om de worp op te slaan.\n");
+							System.out.println("of selecteer welke dobbelstenen je wilt vasthouden.\nType '0' voor geen, anders bijvoorbeeld positie '13'");
+							String invoer = sc.next();
+							if(invoer.equals("p")){
+								opslaanWorp(spelerIndex);
+								spelers.get(spelerIndex).toonWorpGeschiedenis(spelerIndex, beurtenIndex);
+								spelers.get(spelerIndex).aantalWorpen = 3;
+								System.out.println("");
+							} else {
+								vasthouden(invoer);
+							}
+						}
+					}
 				}
 			}
-			opslaanWorp();
-			speler.toonWorpGeschiedenis();
-			speler.aantalWorpen = 0;
-			System.out.println("");
 		}		
 	}
 	
 	
 	void vasthouden(String invoerVasthouden) {
-		resetBlokkeerArray();
 		for(int i = 0; i < invoerVasthouden.length(); i++){
 			int blokkeerIndex = Integer.parseInt(Character.toString(invoerVasthouden.charAt(i)));  //Character.toString om parseInt(string) werkend te krijgen
 			if(blokkeerIndex > 0 && blokkeerIndex < 6){
@@ -65,17 +79,17 @@ public class YahtzeeSpel {
 		}
 	}
 	
-	void opslaanWorp(){
+	void opslaanWorp(int a){
 		Worp worp = new Worp();
 		for(int q = 0; q < dobbelstenen.size(); q++){
 			worp.setWorpUitslag(dobbelstenen.get(q).ogenWorp, q);
 		}
-		speler.setWorpGeschiedenis(worp);
+		spelers.get(a).setWorpGeschiedenis(worp);
 	}
 	
-	void werpDobbelstenen() {
-		speler.aantalWorpen++;
-		System.out.println("Worp " + speler.aantalWorpen + ":");
+	void werpDobbelstenen(int a, int b) {
+		spelers.get(a).aantalWorpen++;
+		System.out.println("\nWorp " + spelers.get(a).aantalWorpen + "/3 van beurt " + (b + 1) + "/" + maxAantalBeurten + " van " + spelers.get(a).naam + ":");
 		for(Dobbelsteen dobbelsteen : dobbelstenen){
 			if(blokkeerArray[iteratorBlokkeerArray] == 0){
 				dobbelsteen.werpen();
@@ -86,7 +100,18 @@ public class YahtzeeSpel {
 				iteratorBlokkeerArray++;
 			}
 		}
-		iteratorBlokkeerArray = 0;
+		resetBlokkeerArray();
+	}
+		
+	void toevoegenSpelers(){
+		System.out.println("Welkom bij Yahtzee!\n\nHoeveel spelers wil je toevoegen?");
+		int aantalSpelers = sc.nextInt();
+		for(int x = 0; x < aantalSpelers; x++){
+			System.out.println("Speler " + (x+1) + " voer je naam in: ");
+			Speler speler = new Speler();
+			speler.naam = sc.next();
+			spelers.add(speler);
+		}
 	}
 }		
 
